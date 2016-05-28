fcea4cfd8d930be4f469372272a0145792afe16c
Benjamin White <whtouche@Benjamins-MacBook-Pro.local>
2016-05-28 12:54:02 -0400

    fix(index): restore commented out markup
    restore markup for index.html that was commented out while trying to debug test failures

--- app/index.js
+++ app/index.js
@@ -1,15 +1,15 @@
-// import ReactDOM from 'react-dom';
-// import routes from './config/routes';
-
-// ReactDOM.render(
-//   routes,
-//   document.getElementById('app')
-// );
-
-import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import {BeerListContainer} from './components';
+import routes from './config/routes';
 
 ReactDOM.render(
-  <BeerListContainer/>,
-  document.querySelector('#root'));
+  routes,
+  document.getElementById('app')
+);
+
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+// import {BeerListContainer} from './components';
+
+// ReactDOM.render(
+//   <BeerListContainer/>,
+//   document.querySelector('#root'));

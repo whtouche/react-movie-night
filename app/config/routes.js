2d6f3e3bc93544826ac6b7c8d902b0d8354b2eda
Benjamin White <whtouche@Benjamins-MacBook-Pro.local>
2016-06-16 22:02:24 -0400

    clean up routes

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import SearchContainer from 'containers/Search/SearchContainer';
-import Home from 'components/Home/Home';
 
 const routes = (
   <Router history={browserHistory}>
-    <Route path='/' component={SearchContainer}>
-      <IndexRoute component={Home} />
-    </Route>
+    <Route path='/' component={SearchContainer} />
   </Router>
 );
 
